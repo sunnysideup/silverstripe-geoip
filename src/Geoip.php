d7c88c19e8992f69024e41afeeab3756b69706a3
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-04-11 22:31:09 +1200

    PATCH: automated linter

--- src/Geoip.php
+++ src/Geoip.php
@@ -423,7 +423,7 @@ class Geoip
             $response = self::ip2country($url);
 
             if (! $response && $expectedResponse) {
-                user_error("ip2country_check failed sanity check: ip2country({$url}) returned false. Expected code: '".print_r($expectedResponse, 1)."'", E_USER_WARNING);
+                user_error("ip2country_check failed sanity check: ip2country({$url}) returned false. Expected code: '" . print_r($expectedResponse, 1) . "'", E_USER_WARNING);
                 $status = false;
             } elseif ($response !== $expectedResponse) {
                 user_error("ip2country_check failed sanity check: ip2country({$url}) returned code: '{$response['code']}/{$response['name']}'. Expected code: '{$expectedResponse['code']}/{$expectedResponse['name']}'", E_USER_WARNING);
